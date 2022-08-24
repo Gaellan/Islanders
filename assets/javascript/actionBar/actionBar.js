22bfe1ad6b3cff233680c4bf81adba3be6ae65b0
Gaellan <doucet.mari@gmail.com>
2022-08-24 13:28:10 +0200

    taking out the equipment button

--- assets/javascript/actionBar/actionBar.js
+++ assets/javascript/actionBar/actionBar.js
@@ -1,9 +1,7 @@
 import { createMinimapButton } from "./minimap/minimap.js";
 import { createInventoryButton } from "./inventory/inventory.js";
-import { createEquipmentButton } from "./equipment/equipment.js";
 
 export default function loadActionBar() {
     createMinimapButton();
     createInventoryButton();
-    createEquipmentButton();
 }
--- assets/javascript/actionBar/equipment/equipment.js
+++
@@ -1,7 +0,0 @@
-function createEquipmentButton()
-{
-    let $box = document.querySelector("section.row-start-10.col-start-17");
-    $box.classList.add("equipment-button");
-}
-
-export { createEquipmentButton };
